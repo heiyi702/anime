cee80c63e5dd73a1161d5137a9971d8ca2962f55
LUXIAOXI\Administrator <heiyi702@163.com>
2019-07-01 18:29:12 +0800

    ‘router重定向’

--- src/router.js
+++ src/router.js
@@ -9,19 +9,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // },
     {
       path: '/login',
       name: 'Login',
@@ -51,6 +38,7 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+      redirect: { name: 'Login' },
       children: [
         {
           path: 'anime/:type',
